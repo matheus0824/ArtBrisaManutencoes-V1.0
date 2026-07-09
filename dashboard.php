6945fc2c5e55bbb3020b80a03182962617acd2d5
Matheus <matheus.fariaspaz@gmail.com>
2026-07-08 21:10:20 -0300

    criacao de pagina READ-ONLY para visitantes

---
+++ dashboard.php
@@ -0,0 +1,41 @@
+<?php
+require_once 'conexao.php';
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$stmt = $conexao->prepare("SELECT * FROM equipamentos");
+
+$stmt->execute();
+
+$equipamentos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<link rel="stylesheet" href="style.css">
+
+<?php
+echo "<table>";
+echo    "<tr>";
+echo        "<th>Nº da Máquina</th>";
+echo        "<th>Tipo Equipamento</th>";
+echo        "<th>Data da última limpeza</th>";
+echo    "</tr>";
+
+
+
+foreach ($equipamentos as $equipamentoIndividual) {
+    echo "<tr>";
+
+    echo "<td>" . $equipamentoIndividual['num_maquina'] . "</td>";
+    echo "<td>" . $equipamentoIndividual['tipo_equipamento'] . "</td>";
+    echo "<td>" . date('d/m/Y', strtotime($equipamentoIndividual['status'])). "</td>";
+    
+    echo "</tr>";
+};
+
+echo "</table>";
+
+?>
+
+
+
+
