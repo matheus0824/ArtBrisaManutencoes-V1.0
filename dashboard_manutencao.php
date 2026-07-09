b037fde8d65afffa793c899fc0df6944eb9ba4d2
Matheus <matheus.fariaspaz@gmail.com>
2026-07-09 00:37:56 -0300

    style: adiciona visual responsivo em cards e cores das bolinhas de status

---
+++ dashboard_manutencao.php
@@ -0,0 +1,66 @@
+<?php
+require_once 'conexao.php';
+include 'header.php';
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$stmt = $conexao->prepare("SELECT * FROM equipamentos ORDER BY status DESC, id DESC");
+
+$stmt->execute();
+
+$equipamentos = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
+
+<link rel="stylesheet" href="style.css">
+
+<?php
+echo "<table>";
+echo    "<tr>";
+echo        "<th>Nº da Máquina</th>";
+echo        "<th>Tipo Equipamento</th>";
+echo        "<th>Data da última limpeza</th>";
+echo    "</tr>";
+
+
+
+foreach ($equipamentos as $equipamentoIndividual) {
+    $data_manutencao = new DateTime($equipamentoIndividual['status']);
+    $hoje = new DateTime();
+
+    $diferenca = $data_manutencao->diff($hoje);
+
+    $total_meses = ($diferenca->y * 12) + $diferenca->m;
+
+    if ($total_meses >= 3) {
+        $classe_bolinha = "vermelha";
+    } else {
+        $classe_bolinha = "verde";
+    }
+
+    $data_formatada = $data_manutencao->format('d/m/Y');
+
+    echo "<tr>";
+
+    echo "<td>" . $equipamentoIndividual['num_maquina'] . "</td>";
+
+    echo "<td>" . $equipamentoIndividual['tipo_equipamento'] . "</td>";
+
+
+    echo "<td>
+        <div class='status-container'>
+            <span class='bolinha $classe_bolinha'></span>
+            $data_formatada
+        </div>
+    </td>";
+    
+
+    echo "</tr>";
+};
+
+echo "</table>";
+
+?>
+
+
+
+
---
+++ header.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<nav>
+    <a href="cadastro.php">CADASTRO</a>
+    <a href="listar.php">MÁQUINAS</a>
+    <a href="dashboard_manutencao.php">MANUTENÇÔES</a>
+</nav>
