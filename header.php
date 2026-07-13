1858173de895bb9f137da9f71bce0245f66be07f
Matheus <matheus.fariaspaz@gmail.com>
2026-07-13 08:08:10 -0300

    separando classes com div e lista ordenadas no header

--- header.php
+++ header.php
@@ -1,7 +1,14 @@
 <link rel="stylesheet" href="style.css">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<nav>
-    <a href="cadastro.php">CADASTRO</a>
-    <a href="listar.php">MÁQUINAS</a>
-    <a href="dashboard_manutencao.php">MANUTENÇÔES</a>
+
+
+<nav class="navbar">
+    <div class="logo">
+        <img src="img/logo.png" alt="Logo da Empresa"></div>
+        
+    <ul class="nav-links">
+        <li><a href="cadastro.php">CADASTRO</a></li>
+        <li><a href="listar.php">MÁQUINAS</a></li>
+        <li><a href="dashboard_manutencao.php">MANUTENÇÔES</a></li>
+    </ul>
 </nav>
