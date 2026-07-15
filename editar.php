53709b67758a8f72ac181e96802bc9c68b8eac3f
Matheus <matheus.fariaspaz@gmail.com>
2026-07-14 21:24:51 -0300

    alteracao de formulario html de edicao das maquinas

--- editar.php
+++ editar.php
@@ -20,25 +20,38 @@ $equipamento = $stmt->fetch(PDO::FETCH_ASSOC);
     include 'header.php';
     ?>    
 
-
+    <div class="cadastro-container">
     <form action="atualizar.php" method="post">
-        <input type="hidden" name="id" value="<?php echo $equipamento['id']; ?>">
+        <h2>Editar Equipamento</h2>
 
+        <div class="campo-grupo">
+            <label>ID da Máquina:</label>
+            <input type="hidden" name="id" value="<?php echo $equipamento['id']; ?>">
+        </div>
+
+        <div class="campo-grupo">
         <input type="text" name="num_maquina" value="<?php echo $equipamento['num_maquina']; ?>" placeholder="Digite o ID da máquina">
-
+        
+  <label>Tipo de equipamento:</label>
         <select name="tipo_equipamento">
+           
             <option value="Climatizador" <?php echo ($equipamento['tipo_equipamento'] == 'Climatizador') ? 'selected' : ''; ?>>Climatizador 160/170</option>
 
             <option value="Exaustor" <?php echo ($equipamento['tipo_equipamento'] == 'Exaustor') ? 'selected' : ''; ?>>Exaustor</option>
 
             <option value="Portátil" <?php echo ($equipamento['tipo_equipamento'] == 'Portátil') ? 'selected' : ''; ?>>Portátil</option>
         </select>
+</div>
 
-        <input type="date" name="status" value="<?php echo $equipamento['status']; ?>">
+        <div class="campo-grupo">
+        <label>Data da limpeza:</label>
+            <input type="date" name="status" value="<?php echo $equipamento['status']; ?>">
+        </div>
 
         <button type="submit">Salvar</button>
 
     </form>
-    
+</div>
+        
 </body>
 </html>
