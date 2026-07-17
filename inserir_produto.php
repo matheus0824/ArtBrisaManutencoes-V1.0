4dd5b6cbc8b54207e0a355352e01396b22b2d743
Matheus <matheus.fariaspaz@gmail.com>
2026-07-16 22:38:20 -0300

    logica de inseri produto que esta em falta

--- inserir_produto.php
+++ inserir_produto.php
@@ -1,4 +1,18 @@
 <?php
-move_uploaded_file($_FILES['foto']['tmp_name'], 'uploads/' . time() . '_' . $_FILES['foto']['name']);
+require_once 'config/conexao.php';
 
+$name = $_POST['nome_produto'];
+$categoria = $_POST['tipo_equipamento'];
+
+$nome_foto_unico = time() . '_' . $_FILES['foto']['name'];
+$caminho_final = 'uploads/' . $nome_foto_unico;
+
+move_uploaded_file($_FILES['foto']['tmp_name'], $caminho_final);
+
+$sql = "INSERT INTO produtos_catalogo (nome, categoria, foto) VALUES (:nome, :categoria, :foto)";
+$stmt = $conexao->prepare($sql);
+
+$stmt->execute([':nome' => $name, ':categoria' => $categoria, ':foto' => $caminho_final]);
+
+header('Location: listar_produtos.php');
 ?>
